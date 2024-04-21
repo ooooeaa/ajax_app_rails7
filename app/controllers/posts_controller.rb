a41b74330bf85449efcac9075fef9ee4e8e5d754
ooooeaa <sodecoo0621@gmail.com>
2024-04-21 19:01:38 -0400

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,9 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(id: "DESC")
   end
-
-  def new
-  end
-
+ 
   def create
-    binding.pry
     post = Post.create(content: params[:content])
-    render json:{post: post }
+    render json:{ post: post }
   end
-end
+ end
